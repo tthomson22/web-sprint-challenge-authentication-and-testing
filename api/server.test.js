153534b20c57f8622e691e90d964cdb422e7f623
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-28 16:19:34 -0700

    setting up tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,22 @@
 // Write your tests here
+const server = require('./server')
+const db = require('../data/dbConfig')
+const request = require('supertest')
+
+beforeAll(async() => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async() => {
+  await db('users').truncate();
+})
+
+//sanity check
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+afterAll(async() => {
+  await db.destroy()
 })
