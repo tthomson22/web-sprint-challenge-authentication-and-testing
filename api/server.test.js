cf6b71e00d4ee78aa32f1d77abc29e50926899ff
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-28 17:46:50 -0700

    tests created

--- api/server.test.js
+++ api/server.test.js
@@ -12,11 +12,52 @@ beforeEach(async() => {
   await db('users').truncate();
 })
 
+afterAll(async() => {
+  await db.destroy()
+})
+
 //sanity check
 test('sanity', () => {
   expect(true).toBe(true)
 })
 
-afterAll(async() => {
-  await db.destroy()
+describe('/register', () => {
+  test('returns status 201', async() => {
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'name', password: '12345' })
+    expect(res.status).toBe(201)
+  })
+  test('returns status 422', async() => {
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'name', password: '' })
+    expect(res.status).toBe(422)
+  })
+})
+
+describe('/login', () => {
+  test('returns status 201', async() => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'name', password: '12345' })
+    expect(res.status).toBe(201)
+  })
+  test('returns status 422', async() => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'name', password: '' })
+    expect(res.status).toBe(422)
+  })
+})
+
+describe('/jokes', () => {
+  test('resturns status', async () => {
+    const res = await request(server).get('/api/jokes')
+    expect(res.status).toBe(401)
+  })
+  test('returns message', async () => {
+    const res = await request(server).get('/api/jokes')
+    expect(res.body.message).toBe('token required')
+  })
 })
